9db1a7e10081ccb0ba77db7ac14d58e99d4987f1
Artem Manchenkov <artyom@manchenkoff.me>
2019-12-09 22:21:24 +0300

    readme & homework update

--- basic/first/03-functions.py
+++ basic/first/03-functions.py
@@ -5,3 +5,16 @@
 #
 #  Работа с функциями, аргументы и возвращаемое значение
 #
+
+original_users = ["Artur", "John", "Jack"]
+
+
+def print_users(users):
+    for user in users:
+        print("Hello, " + user)
+
+
+another_users = ["Kate", "Gatlin", "Chris"]
+
+print_users(original_users)
+print_users(another_users)
--- basic/first/04-classes.py
+++ basic/first/04-classes.py
@@ -5,3 +5,13 @@
 #
 #  Основы ООП, класс, объект, метод и атрибут
 #
+
+class User:
+    first_name: str
+    last_name: str
+
+
+john = User()
+john.first_name = "John"
+
+print(john.first_name)
