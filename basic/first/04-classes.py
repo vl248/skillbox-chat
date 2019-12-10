f6bf7303ea8a9b540997c330bf358c69b875f9a0
Artem Manchenkov <artyom@manchenkoff.me>
2019-12-10 22:21:17 +0300

    day 02 update

--- basic/first/04-classes.py
+++ basic/first/04-classes.py
@@ -10,8 +10,21 @@ class User:
     first_name: str
     last_name: str
 
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
 
-john = User()
-john.first_name = "John"
+    def full_name(self):
+        # return self.first_name + self.last_name
+        return f"Fullname: {self.first_name} {self.last_name}"
 
-print(john.first_name)
+
+john = User("John", "Doe")
+# john.first_name = "John"
+
+artur = User("Artur", "Doe")
+# artur.first_name = "Artur"
+
+# print(john.first_name, john.last_name)
+# print(artur.first_name, artur.last_name)
+print(john.full_name())
--- basic/second/02-twisted.py
+++ basic/second/02-twisted.py
@@ -13,3 +13,18 @@
 #  1. pip install twisted - установка пакета
 #  2. from twisted import ... - подключить в файле .py
 #
+from twisted.internet import protocol, reactor, endpoints
+
+
+class Echo(protocol.Protocol):
+    def dataReceived(self, data):
+        self.transport.write(data)
+
+
+class EchoFactory(protocol.Factory):
+    def buildProtocol(self, addr):
+        return Echo()
+
+
+endpoints.serverFromString(reactor, "tcp:1234").listen(EchoFactory())
+reactor.run()
