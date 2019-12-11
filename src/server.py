2707f378fb6a1768c0e3d29e956086e552341f11
vl <3doff@mail.ru>
2019-12-11 18:51:35 +0300

    Выполнены ДЗ второго дня.

--- src/server.py
+++ src/server.py
@@ -15,6 +15,8 @@ from twisted.protocols.basic import LineOnlyReceiver
 class ServerProtocol(LineOnlyReceiver):
     factory: 'Server'
     login: str = None
+    logins: list = []
+    content_log: list = []
 
     def connectionMade(self):
         # Потенциальный баг для внимательных =)
@@ -28,6 +30,9 @@ class ServerProtocol(LineOnlyReceiver):
 
         if self.login is not None:
             content = f"Message from {self.login}: {content}"
+            self.content_log.append(content)
+            if len(self.content_log) > 10:
+                del self.content_log[0]
 
             for user in self.factory.clients:
                 if user is not self:
@@ -36,10 +41,19 @@ class ServerProtocol(LineOnlyReceiver):
             # login:admin -> admin
             if content.startswith("login:"):
                 self.login = content.replace("login:", "")
-                self.sendLine("Welcome!".encode())
+                if self.login not in self.logins:
+                    self.sendLine("Welcome you, ".encode() + self.login.encode())
+                    self.logins.append(self.login)
+                    self.send_history()
+                else:
+                    self.sendLine("Login ".encode() + self.login.encode() + " already taken! Choose another.".encode())
+                    self.transport.loseConnection()
             else:
                 self.sendLine("Invalid login".encode())
 
+    def send_history(self):
+        for history_line in self.content_log:
+            self.sendLine(history_line.encode())
 
 class Server(ServerFactory):
     protocol = ServerProtocol
